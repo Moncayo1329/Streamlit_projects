0f54999c03ad556bfe1a5a8a56ae14769d210720
Moncayo1329 <moncayomichael23@gmail.com>
2026-03-24 16:13:20 +0000

    projects4

--- Project4/main.py
+++ Project4/main.py
@@ -21,13 +21,13 @@ if st.button('Agregar tarea'):
 st.subheader('📋 Tus tareas')
 if not st.session_state.tasks:
     st.info('No tienes tareas aun tareas')
-    else:
+else:
         for i, task in enumerate(st.session_state.tasks):
             col1, col2 = st.columns([0.8,0.2])
-                    with col1:
+        with col1:
             checked = st.checkbox(task["text"], value=task["done"], key=f"check_{i}")
             st.session_state.tasks[i]["done"] = checked
-                             with col2:
+        with col2:
             if st.button("🗑️", key=f"delete_{i}"):
                 del st.session_state.tasks[i]
                 st.rerun()
